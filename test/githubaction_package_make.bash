8f6262654c9beed3fc802e87d58f33114f985725
Takaaki Yokoyama <yoko99@hat.hi-ho.ne.jp>
2023-02-02 06:20:14 +0000

    wall_stop.launsh was modified

--- test/githubaction_package_make.bash
+++ test/githubaction_package_make.bash
@@ -1,6 +1,6 @@
 #!/bin/bash -xve
 
 #copy files from pimouse_ros using git.clone
-#cd ~/work/catkin_ws/src/
+cd ~/work/catkin_ws/src/
 git clone --depth=1 https://github.com/yoko99/pimouse_ros.git
-#cd ~/work/catkin_ws
+cd ~/work/catkin_ws
