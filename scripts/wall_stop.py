894640a35c6e734010d5f3578476f62b1b38bd01
Takaaki Yokoyama <yoko99@hat.hi-ho.ne.jp>
2023-02-03 06:08:31 +0000

    wall_stop.py was modified

--- scripts/wall_stop.py
+++ scripts/wall_stop.py
@@ -9,7 +9,7 @@ class WallStop():
         self.cmd_vel = rospy.Publisher('/cmd_vel',Twist,queue_size=1)
 
         self.sensor_values = LightSensorValues()
-        rospy.Subscriber('/lightsensors', LightsensorValues, self.callback)
+        rospy.Subscriber('/lightsensors', LightSensorValues, self.callback)
 
     def callback(self, messages):
         self.sensor_values = messages
