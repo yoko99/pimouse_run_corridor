f39967c30d4993a58e068bdb7994a79aa59de13b
Takaaki Yokoyama <yoko99@hat.hi-ho.ne.jp>
2023-02-03 05:58:07 +0000

    wall_stop.py was modified

--- scripts/wall_stop.py
+++ scripts/wall_stop.py
@@ -26,7 +26,7 @@ class WallStop():
 if __name__ == '__main__':
     rospy.init_node('wall_stop')
     rospy.wait_for_service('/motor_on')
-    rospy.wait_for_service("/motor_off")
+    rospy.wait_for_service('/motor_off')
     rospy.on_shutdown(rospy.ServiceProxy('/motor_off',Trigger).call)
     rospy.ServiceProxy('motor_on',Trigger).call()
     WallStop().run()
