1458cf0ccc83e454ea5492c1e28f1e62198eee57
Takaaki Yokoyama <yoko99@hat.hi-ho.ne.jp>
2023-02-03 06:20:47 +0000

    travis_test_wall_stop.py was modified

--- test/travis_test_wall_stop.py
+++ test/travis_test_wall_stop.py
@@ -5,7 +5,7 @@ import time
 
 class WallStopTest(unittest.TestCase):
     def set_and_get(self,lf,ls,rs,rf):
-        with open("/def/rtlihgtsensor0","w") as f:
+        with open("/def/rtlightsensor0","w") as f:
             f.write("%d %d %d %d\n" % (rf,rs,ls,lf))
 
         time.sleep(0.3)
