9e183ddcd14005a94a9aa1b60dc2e08d887b1b10
Takaaki Yokoyama <yoko99@hat.hi-ho.ne.jp>
2023-02-16 07:10:09 +0000

    test.yaml file was modified

--- test/travis_test_wall_stop.py
+++ test/travis_test_wall_stop.py
@@ -6,7 +6,7 @@ import time
 class WallStopTest(unittest.TestCase):
     def set_and_get(self,lf,ls,rs,rf):
         with open("/dev/rtlightsensor0","w") as f:
-            f.write("%d %d %d %d\n" % (rf,rs,ls,lf))
+            f.write("%d %d %d %d\n" %(rf,rs,ls,lf))
 
         time.sleep(0.3)
 
