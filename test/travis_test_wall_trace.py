e1a0e72a004af9d368c3668e73653c553b4b489f
Takaaki Yokoyama <yoko99@hat.hi-ho.ne.jp>
2023-02-16 08:25:02 +0000

    travis_test_wall_trace.py was modified

--- test/travis_test_wall_trace.py
+++ test/travis_test_wall_trace.py
@@ -25,7 +25,7 @@ class WallTraceTest(unittest.TestCase):
         self.assertTrue(left == right != 0, "stop wrong by side sensors")
 
         left, right = self.set_and_get(0, 10, 0, 0) #curve to left
-        self.assertTrune(left < right, "don't curve to left")
+        self.assertTrue(left < right, "don't curve to left")
 
         left, right = self.set_and_get(0,200,0,0) #tcueve to right
         self.assertTrue(left < righ, "don't curve right")
