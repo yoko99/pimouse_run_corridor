021f02d4b9b02b9ec5fae6796e1d4551d58e7a90
Takaaki Yokoyama <yoko99@hat.hi-ho.ne.jp>
2023-02-16 05:29:13 +0000

    travis_test_wall_trace.py was updated

--- test/travis_test_wall_trace.py
+++ test/travis_test_wall_trace.py
@@ -35,6 +35,6 @@ class WallTraceTest(unittest.TestCase):
 
 if __name__ == '__main__':
     time.sleep(3)
-    rospy.init_node('travis_test_wall_stop')
-    rostest.rosrun('pimouse_run_corridor', 'travis_test_wall_stop',WallStopTest)
+    rospy.init_node('travis_test_wall_trace')
+    rostest.rosrun('pimouse_run_corridor', 'travis_test_wall_trace',WallStopTest)
 
