f5d92f22814011bf2bb846145f619b7d22dfa25c
Takaaki Yokoyama <yoko99@hat.hi-ho.ne.jp>
2023-02-16 08:17:49 +0000

    wall_trace.py was modified

--- scripts/wall_trace.py
+++ scripts/wall_trace.py
@@ -28,8 +28,8 @@ class WallTrace():
             elif data.linear.x  <=  0.2: data.linear.x = 0.2
             elif data.linear.x  >=  0.8: data.linear.x = 0.8
         
-            if data.linear.x < 0.2: data.linear.x = 0.0 
-            elif s.left_side < 10:  data.linear.x = 0.0
+            if data.linear.x < 0.2: data.angular.z = 0.0 
+            elif s.left_side < 10:  data.angular.z = 0.0
             else:
                 target = 50
                 error = (target - s.left_side)/50.0
