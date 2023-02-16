cc7394d80a75e07b9ef100c565aac0d9fc366987
Takaaki Yokoyama <yoko99@hat.hi-ho.ne.jp>
2023-02-16 05:21:54 +0000

    travis_test_wall_trace.py was updated

---
+++ test/travis_test_wall_trace.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import unittest, rostest
+import rosnode, rospy
+import time
+
+class WallTraceTest(unittest.TestCase):
+    def set_and_get(self,lf,ls,rs,rf):
+        with open("/dev/rtlightsensor0","w") as f:
+            f.write("%d %d %d %d\n" % (rf,rs,ls,lf))
+
+        time.sleep(0.3)
+
+        with open("/dev/rtmotor_raw_l0","r") as lf,\
+             open("/dev/rtmotor_raw_r0","r") as rf:
+            left = int(lf.readline().rstrip())
+            right = int(rf.readline().rstrip())
+
+        return left, right
+
+    def test_io(self):
+        left, right = self.set_and_get(400,00,0,100) #total: 600
+        self.assertTrue(left == right == 0, "can't stop")
+
+        left, right = self.set_and_get(0,5,1000,0) #side direction is not trigger of stop
+        self.assertTrue(left == right < != 0, "stop wrong by side sensors")
+
+        left, right = self.set_and_get(0, 10, 0, 0) #curve to left
+        self.assertTrune(left < right, "don't curve to left")
+
+        left, right = self.set_and_get(0,200,0,0) #tcueve to right
+        self.assertTrue(left < righ, "don't curve right")
+
+        left, right = self.set_and_get(0,5,0,0) #don't control when far from a wall
+        self.assertTrue(left == right, "curve wrongly")
+
+if __name__ == '__main__':
+    time.sleep(3)
+    rospy.init_node('travis_test_wall_stop')
+    rostest.rosrun('pimouse_run_corridor', 'travis_test_wall_stop',WallStopTest)
+
